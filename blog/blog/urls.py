7d5fef8ff4cb6b374fa84540f7292b6b1e587ec5
Denis Didenko <denis98didenko@gmail.com>
2022-09-11 16:16:44 +0300

    Created model User / Added path to media folder

--- blog/blog/urls.py
+++ blog/blog/urls.py
@@ -1,6 +1,8 @@
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 
+from blog import settings
 from board.views import page_not_found
 
 urlpatterns = [
@@ -8,5 +10,7 @@ urlpatterns = [
     path('board/', include('board.urls')),
 ]
 
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 handler404 = page_not_found
--- blog/board/models.py
+++ blog/board/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+
+class User(models.Model):
+    name = models.CharField(max_length=10)
+    photo = models.ImageField(upload_to="photos/%Y/%m/%d/")
+    creation_date = models.DateTimeField(auto_now_add=True)
