1b227e61c75cd30777a41d4a3bbedb66bc93a816
Denis Didenko <denis98didenko@gmail.com>
2022-09-13 20:18:29 +0300

    Added pages for side menu

--- blog/board/urls.py
+++ blog/board/urls.py
@@ -3,6 +3,11 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('', index),
+    path('', index, name='login'),
     path('my_page/', main_page),
+    path('create_note/', create_note, name='create_note'),
+    path('my_notes/', my_notes, name='my_notes'),
+    path('draft/', draft, name='draft'),
+    path('subscriptions/', subscriptions, name='subscriptions'),
+    path('subscribers/', subscribers, name='subscribers'),
 ]
--- blog/board/views.py
+++ blog/board/views.py
@@ -3,14 +3,40 @@ from django.shortcuts import render
 
 from .models import *
 
+menu = [{'title': "Создать запись", 'url_name': "create_note"},
+        {'title': "Мои записи", 'url_name': "my_notes"},
+        {'title': "Черновик", 'url_name': "draft"},
+        {'title': "Подписки", 'url_name': "subscriptions"},
+        {'title': "Подписчики", 'url_name': "subscribers"}]
+
 
 def index(request):
-    return render(request, '')
+    return render(request, 'board/index.html')
 
 
 def main_page(request):
     user = User.objects.get(pk=1)
     return render(request, 'board/main_page.html', {'user': user})
+
+
+def create_note(request):
+    return HttpResponse("Создать запись")
+
+
+def my_notes(request):
+    return HttpResponse("Мои записи")
+
+
+def draft(request):
+    return HttpResponse("Черновик")
+
+
+def subscriptions(request):
+    return HttpResponse("Подписки")
+
+
+def subscribers(request):
+    return HttpResponse("Подписчики")
 
 
 def page_not_found(request, exception):
