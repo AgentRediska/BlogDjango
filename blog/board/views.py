10fc78f5f53010c319507176c368e9c002ecd934
Denis Didenko <denis98didenko@gmail.com>
2022-09-13 13:03:45 +0300

    Added way to static directory

--- blog/board/views.py
+++ blog/board/views.py
@@ -1,13 +1,16 @@
 from django.http import HttpResponse, HttpResponseNotFound
 from django.shortcuts import render
 
+from .models import *
+
 
 def index(request):
     return render(request, '')
 
 
 def main_page(request):
-    return render(request, 'board/main_page.html')
+    user = User.objects.get(pk=1)
+    return render(request, 'board/main_page.html', {'user': user})
 
 
 def page_not_found(request, exception):
