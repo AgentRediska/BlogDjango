629e150d649169858c9e1343fb30f32ca96df9f9
Denis Didenko <denis98didenko@gmail.com>
2022-10-17 14:28:14 +0300

    Deleted unused tag

--- blog/board/templatetags/board_tags.py
+++ blog/board/templatetags/board_tags.py
@@ -9,22 +9,6 @@ def get_user_by_pk(user_pk):
     return User.objects.get(pk=user_pk)
 
 
-@register.inclusion_tag('board/inclusion/subscription_notes.html')
-def show_note_dict(user_pk):
-    subsc = Subscription.objects.filter(user_id=user_pk)
-    sub_user = []
-    note_dict = {}
-    for sub in subsc:
-        sub_user.append(User.objects.get(pk=sub.subscription_id))
-        notes = Note.objects.filter(creator=sub.subscription_id)
-        for note in notes:
-            author = User.objects.get(pk=note.creator.pk)
-            like = Like.objects.filter(note=note).count()
-            dislike = Dislike.objects.filter(note=note).count()
-            note_dict[note] = (author, like, dislike)
-    return {"note_dict": note_dict}
-
-
 @register.inclusion_tag('board/inclusion/subscriptions.html')
 def show_subscription_list(user_pk, search_text=''):
     print(search_text)
